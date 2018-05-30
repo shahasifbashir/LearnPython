52a7c2ec4dc6459e62a3ed2e76053f89ea47a6c8
shahasifbashir <shaharslan3339@gmail.com>
2018-05-30 14:58:14 +0530

    Added even odd example

---
+++ even odd/evenodd.py
@@ -0,0 +1,25 @@
+print("Even odd Example")
+
+#ask for the input
+
+print("Enter a number")
+
+num = int(input())
+
+#check if the number is even or odd
+
+if (num%2==0):
+    print("You have entered {} an the number  is EVEN".format(num))
+else:
+        print("You have entered {} an the number  is ODD".format(num))
+
+#Ask for the number that is and check if it is divisible by 4
+
+print("Enter a number")
+
+num = int(input())
+
+if (num%4==0):
+    print("You have entered {} an the number  is divisible by 4".format(num))
+else:
+        print("You have entered {} an the number  is not divisible by 4".format(num))
