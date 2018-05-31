282d8b580aa62b5ead179ced964413d39bce8338
shahasifbashir <shaharslan3339@gmail.com>
2018-05-31 13:37:28 +0530

    Added the divisor example

---
+++ Divisor/Divisors.py
@@ -0,0 +1,10 @@
+#This program asks user for a number and the outputs all the divisors of that number
+#Ask user for a n input
+num = int (input("Input an integer : "))
+# now create a list of all possible divisors
+
+myList = range(2,(num//2+1))
+
+for item in myList:
+    if(num%item==0):
+        print(item,end=" ")
